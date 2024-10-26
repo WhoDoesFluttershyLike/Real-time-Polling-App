0bd5fa1b75b6e3fbb389a43a8ef242781b6891b8
007 <mikaasaa@mail.ru>
2024-10-26 22:44:26 +0600

    # WebSocket Configuration

---
+++ src/test/java/com/example/Real_time/Polling/App/RealTimePollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Real_time.Polling.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimePollingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
